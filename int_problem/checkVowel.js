cfc1e653b9e58206882d95730c3cb20410079b6c
MD. yasin Arafat Mubin <mubinulislam14@gmail.com>
2025-10-09 02:40:16 +0600

    solve checkVowel

---
+++ int_problem/checkVowel.js
@@ -0,0 +1,11 @@
+const checkVowel = (str) => {
+  let vowel = 0;
+  for (let i = 0; i < str.length; i++) {
+    if ("aeiou".includes(str[i])) {
+      vowel += 1;
+    }
+  }
+  console.log(vowel);
+};
+
+checkVowel("hello");
