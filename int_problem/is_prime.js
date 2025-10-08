b58de5375438b37da4af4ba299ba550b0a352cfc
MD. yasin Arafat Mubin <mubinulislam14@gmail.com>
2025-10-09 04:01:22 +0600

    solve find Prime num

---
+++ int_problem/is_prime.js
@@ -0,0 +1,16 @@
+const findPrime=(num)=>{
+    for(let i = 2; i<=num ;i++){
+        let isPrime = true
+        for(let j = 2; j<i; j++){
+            if(i %j === 0){
+                isPrime = false
+                break
+            }
+        }
+        if(isPrime){
+            console.log(i)
+        }
+    }
+}
+
+findPrime(10)
