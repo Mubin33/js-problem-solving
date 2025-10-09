0ea43cf9402ad25b0616d72df6a4052af9e7f60d
MD. yasin Arafat Mubin <mubinulislam14@gmail.com>
2025-10-10 03:51:50 +0600

    resolve day4

--- day4.js
+++ day4.js
@@ -1,17 +1,31 @@
 
 // ekta array pass korbe jekhane kisu integer thakbe , sei integer gulo check kora hobe je secuence onu jai ase kina. jodi thake tahole true dibe r jodi na thake tahole false dibe.   
-array = [1, 2, 3, 4]
+array = [3, 4]
 array1 = [1, 3, 2, 3, 4]
 array2 = [1, 3, 2]
 
-const secuence = (arr) => {
-    for (let i = 1; i < arr.length; i++) { // arr take for loop er shahajje lelement gulo nicche array er length porjonto
-        if (arr[i] < arr[i - 1]) { // eikhane check kortase je bortoman i er value ta ki ager i er value theke choto kina. 
+// const secuence = (arr) => {
+//     for (let i = 1; i < arr.length; i++) { // arr take for loop er shahajje lelement gulo nicche array er length porjonto
+//         if (arr[i] < arr[i - 1]) { // eikhane check kortase je bortoman i er value ta ki ager i er value theke choto kina. 
+//             return false
+//         }
+//     }
+//     return true
+// }
+
+
+
+
+const secuence=(arr)=>{ 
+    for(let i = 1; i<arr.length; i++){
+        if(arr[i] < arr[i-1]){
             return false
         }
     }
     return true
 }
+
+
 
  
 
